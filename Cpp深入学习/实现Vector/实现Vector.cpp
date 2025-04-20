7e974f9fc50455e38cd0486560b71e5dfec2f750
wei <2391428860@qq.com>
2025-04-20 22:58:51 +0800

    1

--- Cpp深入学习/实现Vector/实现Vector.cpp
+++ Cpp深入学习/实现Vector/实现Vector.cpp
@@ -1,5 +1,7 @@
 // 实现Vector
+#include <algorithm>
 #include <cstdlib>
+#include <cstring>
 #include <stdio.h>
 
 
@@ -7,10 +9,42 @@ struct Vector
 {
     int *m_data;
     size_t m_size;
-    Vector(size_t n)
+
+    Vector()
     {
-        m_data = new int[n];
+        m_size = 0;
+        m_data = nullptr;
+    }
+    
+    explicit Vector(size_t n)  // 防止隐式转换，explicit关键字，只能显示调用
+    {
+        m_data = new int[n]{}; // 初始化所有元素为0,如果不加{}，则会是随机值
         m_size = n;
+    }
+
+    void clear()
+    {
+        resize(0);
+    }
+    
+    void resize(size_t n)  
+    {
+        auto old_data = m_data;
+        auto old_size = m_size;
+
+        if (n == 0)
+        {
+            m_data = nullptr;
+            m_size = 0;
+        }
+        else{
+            m_data = new int[n]{}; 
+            m_size = n;
+            if (old_data != nullptr){
+                memcpy(m_data, old_data, std::min(old_size,n * sizeof(int));  // 拷贝旧数据到新数据
+                delete[] old_data;
+            }
+        }
     }
 
     size_t size() const  // size(Vector *this)默认会有，所以要加const
@@ -29,6 +63,12 @@ struct Vector
         return m_data[i];
     }
 
+    Vector(const Vector& other)
+    {
+        m_size = other.m_size;  
+        m_data = new int[m_size];
+        memcpy(m_data, other.m_data, m_size*sizeof(int));
+    }
     
     ~Vector()
     {
@@ -38,6 +78,7 @@ struct Vector
 
 int main(int argc, char* argv[])
 {
+    Vector v;
     Vector arr(3);
     for (size_t i = 0; i < arr.size(); i++)
     {
@@ -46,3 +87,4 @@ int main(int argc, char* argv[])
     system("pause");
     return 0;
 }
+ 
